4b88055bbfe6ebaa7993b7500931181fb8b56bb6
lee88888 <83142062@qq.com>
2016-07-22 10:49:58 +0800

    个人中心页面嵌套

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import fetch from 'isomorphic-fetch';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => res.render('user'));
+
+//个人中心首页
+router.get('/', (req, res, next) => res.render('user/user'));
+
+
+//订单详情 
+router.get('/orderdetails', (req, res, next) => res.render('user/orderdetails'));
+
+//优惠券详情
+router.get('/coupondetails', (req, res, next) => res.render('user/coupondetails'));
+
+
+router.get('/:aid',(req, res, next) => {
+	fetch('http://10.10.16.173/test/selciname/bycinemaID?cinemaID=2')
+	.then(response => response.json())
+	.then(book => {
+		console.log(book);
+		res.render('active/details', { book:book.resl });
+	});
+});
 
 export default router;
