ddfa2a481bd5d85cae3b94b28fa76fdb70ebbb65
bingosir <1457955164@qq.com>
2016-07-25 18:02:06 +0800

    入口session  存储

--- middlewares/setdata.js
+++ middlewares/setdata.js
@@ -1,10 +1,12 @@
 import fetch from 'isomorphic-fetch';
 
 const setdata = ( req, res, next) => {
-	fetch('http://10.10.16.173/test/selciname/bycinemaID?cinemaID=3')
+	fetch(api_url+'selciname/bycinemaID?cinemaID=1')
 	.then(response => response.json())
 	.then(book => {
-		console.log(book);
+		//console.log(book.resl[0]['cinemaid']);
+		req.session.cinemaid = book.resl[0]['cinemaid'];//写入至session   影院id
+
 	});
 	return next();
 	
