bbdd2e41d05f7a02956b59d08d07b1dc0aac8b4e
FalseMask <hfgb_92@163.com>
2016-07-27 16:15:25 +0800

    首页背景高度计算

--- public/home/Flime_init.js
+++ public/home/Flime_init.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
-import { foo } from "./swiper.js"; 
+import { foo } from "./swiper.js";
 var obj_index = 1;
 var obj_index1 = 1;
 $(document).ready(function(){
-	
+
 	$.initData();
-	
+
 });
 var vals = 0;
 var vald = 0;
@@ -20,7 +20,7 @@ function touchChange(index_text){
 		 if(vald==vals){
 			return false;
 		 }else{
-			
+
 			  var htmlLoad = '';
 			  var htmlLoads = '';
 			  var htmlk= '';
@@ -33,7 +33,7 @@ function touchChange(index_text){
 				 success: function(msg){
 				 	//alert(vald)
 				//alert(msg.data.KB);
-				for (var key in msg.resl) {		
+				for (var key in msg.resl) {
 				//alert()
 					if(msg.resl[key].entMovieId==vald){
 						var h3html = '<span class="span1">'+msg.resl[key].movieZname+'</span>';
@@ -42,17 +42,17 @@ function touchChange(index_text){
 							}
 							$('.banner_h2_tex').html(h3html);
 						break;
-					}			
-						
-				}
-				
+					}
 
-					
-					
+				}
+
+
+
+
 				var planHtml = '';
 				var dayHtml = '';
 				var dayI = 0;
-				
+
 				for (key in msg.plans) {
 					//alert(key);
 					if(dayI == 0){
@@ -70,7 +70,7 @@ function touchChange(index_text){
 								planHtml += '<div class="swiper_nav_h3">';
 								planHtml += '<a href="/index.php/index/activity/id/'+msg.plans_date[key][tc].id1+'"><p><span class="span1">'+msg.plans_date[key][tc].name1+'</span><span>'+msg.plans_date[key][tc].name2+'</span></p></a>';
 								planHtml += '</div>';
-							}							
+							}
 							var vp = msg.plans[key];
 							var tc = '';
 							for (vk in vp) {
@@ -97,21 +97,21 @@ function touchChange(index_text){
 											planHtml += '<a href="###">';
 										}
 										//planHtml += '<a href="/index.php/plan/selectSeat/pid/'+vpObj[vok].planId+'/id/'+vpObj[vok].id+'/yc/'+yc+'">';
-										
+
 										planHtml += '<input type="hidden" value='+vpObj[vok].planId+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].id+'>';
 										planHtml += '<input type="hidden" value='+yc+'>';
-										planHtml += '<input type="hidden" value='+vpObj[vok].zday+''+vpObj[vok].zdate+'>';	
+										planHtml += '<input type="hidden" value='+vpObj[vok].zday+''+vpObj[vok].zdate+'>';
 										planHtml += '<input type="hidden" value='+msg.plans_date[key][vk].day+'>';
-										planHtml += '<input type="hidden" value='+vpObj[vok].data.hallName+'>';										
+										planHtml += '<input type="hidden" value='+vpObj[vok].data.hallName+'>';
 
 										planHtml += '<p class="p1">'+vpObj[vok].stimes+'</p>';
 										planHtml += '<p class="p2">结束</p>';
 										planHtml += '<p class="p3">'+vpObj[vok].etimes+'</p>';
-										
+
 										planHtml += '<p class="p4"><span class="span_a1">'+vpObj[vok].data.hallName+'</span> </span class="span_a2">'+vpObj[vok].language+'/'+vpObj[vok].screenType+'</span></p>';
 										planHtml += '<p class="p5">';
-											
+
 											planHtml += '<span class="span2">¥'+vpObj[vok].dis_price+'</span>';
 										planHtml += '</p>';
 										if(msg.plans_date[key][vk].day=='今天'){
@@ -125,22 +125,22 @@ function touchChange(index_text){
 											planHtml += '<a href="/index.php/plan/selectSeat/pid/'+vpObj[vok].planId+'/id/'+vpObj[vok].id+'/yc/'+yc+'">';
 										}
 										//planHtml += '<a href="/index.php/plan/selectSeat/pid/'+vpObj[vok].planId+'/id/'+vpObj[vok].id+'/yc/'+yc+'">';
-										
+
 										planHtml += '<input type="hidden" value='+vpObj[vok].planId+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].id+'>';
 										planHtml += '<input type="hidden" value='+yc+'>';
-										planHtml += '<input type="hidden" value='+vpObj[vok].zday+''+vpObj[vok].zdate+'>';	
+										planHtml += '<input type="hidden" value='+vpObj[vok].zday+''+vpObj[vok].zdate+'>';
 										planHtml += '<input type="hidden" value='+msg.plans_date[key][vk].day+'>';
-										planHtml += '<input type="hidden" value='+vpObj[vok].data.hallName+'>';										
-										
+										planHtml += '<input type="hidden" value='+vpObj[vok].data.hallName+'>';
+
 
 										planHtml += '<p class="p1">'+vpObj[vok].stimes+'</p>';
 										planHtml += '<p class="p2">结束</p>';
 										planHtml += '<p class="p3">'+vpObj[vok].etimes+'</p>';
-										
+
 										planHtml += '<p class="p4"><span class="span_a1">'+vpObj[vok].data.hallName+'</span> </span class="span_a2">'+vpObj[vok].language+'/'+vpObj[vok].screenType+'</span></p>';
 										planHtml += '<p class="p5">';
-											
+
 											planHtml += '<span class="span2">¥'+vpObj[vok].dis_price+'</span>';
 										planHtml += '</p>';
 										if(msg.plans_date[key][vk].day=='今天'){
@@ -163,12 +163,12 @@ function touchChange(index_text){
 							//alert(tc);
 							//dayHtml += '<a href="#"'+cur+' id="'+msg.plans_date[key][tc].date+'">'+msg.plans_date[key][tc].day+'<br>'+msg.plans_date[key][tc].fdate+'</a>';
 							dayHtml += '<li '+cur+' id="'+msg.plans_date[key][tc].date+'" data_type="'+msg.plans_date[key][tc].bs+'"><span class="span1">'+msg.plans_date[key][tc].day+'</span><span class="span2">'+msg.plans_date[key][tc].fdate+'</span></li>';
-							
+
 					planHtml += '</ul>';
 					dayI++;
 				}
 				for (key in msg.noplan) {
-					
+
 					if(msg.noplancount == 4 && key == 1){
 						var nocurli = ' click_li';
 						var nocurul = ' click_ul';
@@ -183,7 +183,7 @@ function touchChange(index_text){
 				var yphd = '';
 				//alert(msg.plans_date[key][tc].bs);
 				if(msg.activity==null){
-					yphd='';				
+					yphd='';
 				}else{
 					yphd='<a href="/index.php/index/activity/id/'+msg.activity.id+'"><p><span>'+msg.activity.name[0]+'</span>'+msg.activity.name[1]+'</p></a>';
 				}
@@ -195,25 +195,25 @@ function touchChange(index_text){
 					$('#dateTabs').show().html(dayHtml);
 					$('#swiper-wrapper').html(planHtml);
 					$('.Null_filme').hide();
-					
-					
+
+
 				}else{
 					$('#swiper-wrapper').html('');
 					$('.swiper-pages').hide();
 					$('.Null_filme').show();
 					$('#dateTabs').hide();
-					
+
 				}
-	
+
 
 
 
 
 				  }
-			 }); 
-			 
-			
-			
+			 });
+
+
+
 
 		 }
 		//$('.swiper-nav .swiper-slide').removeClass('swiper-slide-active').eq(0).addClass('swiper-slide-active');
@@ -230,7 +230,7 @@ function touchChange1(index_text){
 		 if(vald==vals){
 			return false;
 		 }else{
-			
+
 			  var htmlLoad = '';
 			  var htmlLoads = '';
 			  var htmlk= '';
@@ -243,7 +243,7 @@ function touchChange1(index_text){
 				 success: function(msg){
 					 	//alert(vald)
 					//alert(msg.data.KB);
-					for (var key in msg.resl) {		
+					for (var key in msg.resl) {
 					//console.log(msg.resl)
 						if(msg.resl[key].entMovieId==vald){
 							var Time = msg.resl[key].movieBetime;
@@ -269,33 +269,48 @@ function touchChange1(index_text){
 								h3html +='<div class="Btn_gou">';
 									h3html +='<div  class="Btn_input">购票</div>';
 								h3html +='</div>';
-								
-								
 
 
 
 
 
-	    		
-	    	
-	    		
-	    		
-	    		
-	    	
+
+
+
+
+
+
+
+
 								$('.flime_h2').html(h3html);
 							break;
-						}			
-							
+						}
+
 					}
 				  }
-			 }); 
+			 });
+        var fot_hei = $('.footer_ul').height();
+        var win_hei = $(window).height()-fot_hei;
+        var bod_hei = $('body').height()-fot_hei;
+        var hei_ght = 0;
+        if(win_hei<bod_hei){
+          hei_ght = bod_hei;
+        }else{
+           hei_ght = win_hei+fot_hei+5;
+        }
+        console.log(hei_ght)
+        $('body').css({
+          "height":hei_ght
+        })
+
+
 		 }
 	//alert(1111);
 	//console.log(index_text);
 }
 $.extend({
-	
-	  
+
+
 	//初始化数据
 	initData: function() {
 		//alert('系统维护中……预计1小时');return false;
@@ -338,15 +353,15 @@ $.extend({
 				htmlLoad += '<li class="text_li">'+msg.data.xxx+'</li>';
 				htmlLoad += '</ul>';
 				htmlLoad += '</div>';
-				
+
 				htmlLoad += '<p class="BL_G_article_H3_a1_p2">'+msg.data.film_time+'分钟，'+msg.data.film_genre+'</p>';
 				htmlLoad += '</a>';
-				
-				
+
+
 				$('#BL_G_article_H3_a1').html(htmlLoad);*/
 
-				for (var key in msg.resl) {		
-				//alert()			
+				for (var key in msg.resl) {
+				//alert()
 						var h3html = '<span class="span1">'+msg.resl[key].movieZname+'</span>';
 						if(msg.resl[key].entdataKb!=null){
 							h3html += '<span class="span2">'+msg.resl[key].entdataKb+'</span>';
@@ -355,13 +370,13 @@ $.extend({
 					break;
 				}
 				var h2html='';
-				for (var zzz in msg.resl) {		
+				for (var zzz in msg.resl) {
             			 h2html += '<div class="swiper-slide">';
             					h2html += '<input type="hidden" value='+msg.resl[zzz].entMovieId+'>';
             					h2html += '<img src='+msg.resl[zzz].titleImg+' />';
             				h2html += ' </div>';
-						
-						
+
+
 				}
 				$('#ypdetail1').html(h2html);
 
@@ -369,9 +384,9 @@ $.extend({
 
 
 
-				
+
 				//console.log(msg.plans);
-					
+
 				var planHtml = '';
 				var dayHtml = '';
 				var planHtmls=[];
@@ -390,7 +405,7 @@ $.extend({
 							for (vk in vp) {
 								tc = vk;
 							}
-													
+
 							var vp = msg.plans[key];
 							var tc = '';
 							for (vk in vp) {
@@ -414,21 +429,21 @@ $.extend({
 											planHtml += '<a href="###">';
 										}
 										//planHtml += '<a href="/index.php/plan/selectSeat/pid/'+vpObj[vok].planId+'/id/'+vpObj[vok].id+'/yc/'+yc+'">';
-										
+
 										planHtml += '<input type="hidden" value='+vpObj[vok].planId+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].id+'>';
 										planHtml += '<input type="hidden" value='+yc+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].zday+''+vpObj[vok].zdate+'>';
 										planHtml += '<input type="hidden" value='+msg.plans_date[key][vk].day+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].data.hallName+'>';
-										
+
 										planHtml += '<p class="p1">'+vpObj[vok].stimes+'</p>';
 										planHtml += '<p class="p2">结束</p>';
 										planHtml += '<p class="p3">'+vpObj[vok].etimes+'</p>';
-										
+
 										planHtml += '<p class="p4"><span class="span_a1">'+vpObj[vok].data.hallName+'</span> </span class="span_a2">'+vpObj[vok].language+'/'+vpObj[vok].screenType+'</span></p>';
 										planHtml += '<p class="p5">';
-											
+
 											planHtml += '<span class="span2">¥'+vpObj[vok].dis_price+'</span>';
 										planHtml += '</p>';
 										if(msg.plans_date[key][vk].day=='今天'){
@@ -436,27 +451,27 @@ $.extend({
 										}
 										planHtml += '</li>';
 									}else{
-										
+
 										planHtml += '<li>';
 										if(msg.plans_date[key][vk].day=='今天'){
 											planHtml += '<a href="/index.php/plan/selectSeat/pid/'+vpObj[vok].planId+'/id/'+vpObj[vok].id+'/yc/'+yc+'">';
 										}
 										//planHtml += '<a href="/index.php/plan/selectSeat/pid/'+vpObj[vok].planId+'/id/'+vpObj[vok].id+'/yc/'+yc+'">';
-										
+
 										planHtml += '<input type="hidden" value='+vpObj[vok].planId+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].id+'>';
 										planHtml += '<input type="hidden" value='+yc+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].zday+''+vpObj[vok].zdate+'>';
 										planHtml += '<input type="hidden" value='+msg.plans_date[key][vk].day+'>';
 										planHtml += '<input type="hidden" value='+vpObj[vok].data.hallName+'>';
-										
+
 										planHtml += '<p class="p1">'+vpObj[vok].stimes+'</p>';
 										planHtml += '<p class="p2">结束</p>';
 										planHtml += '<p class="p3">'+vpObj[vok].etimes+'</p>';
-										
+
 										planHtml += '<p class="p4"><span class="span_a1">'+vpObj[vok].data.hallName+'</span> </span class="span_a2">'+vpObj[vok].language+'/'+vpObj[vok].screenType+'</span></p>';
 										planHtml += '<p class="p5">';
-											
+
 											planHtml += '<span class="span2">¥'+vpObj[vok].dis_price+'</span>';
 										planHtml += '</p>';
 										if(msg.plans_date[key][vk].day=='今天'){
@@ -478,16 +493,16 @@ $.extend({
 							}
 							//alert(msg.plans_date[key][tc].bs);
 							//dayHtml += '<a href="#"'+cur+' id="'+msg.plans_date[key][tc].date+'">'+msg.plans_date[key][tc].day+'<br>'+msg.plans_date[key][tc].fdate+'</a>';
-							
-							
+
+
 							dayHtml += '<li '+cur+' id="'+msg.plans_date[key][tc].date+'" data_type="'+msg.plans_date[key][tc].bs+'"><span class="span1">'+msg.plans_date[key][tc].day+'</span><span class="span2">'+msg.plans_date[key][tc].fdate+'</span></li>';
-							
+
 					planHtml += '</ul>';
 					//alert(planHtml);
 					dayI++;
 				}
 				for (key in msg.noplan) {
-					
+
 					if(msg.noplancount == 4 && key == 1){
 						var nocurli = ' click_li';
 						var nocurul = ' click_ul';
@@ -499,13 +514,13 @@ $.extend({
 					dayHtml += '<li class="'+nocurli+'"><span class="span1">'+msg.noplan[key].day+'</span><span class="span2">'+msg.noplan[key].fdate+'</span></li>';
 					planHtml += '<ul class="list_li_div_ul '+nocurul+'" style="text-align:center">暂无排期！</ul>';
 				}
-				
+
 				$('.swiper_nav_h3').html(yphd);
 				if(planHtml!=''){
 					$('#dateTabs').show().html(dayHtml);
 					$('#swiper-wrapper').show().html(planHtml);
 					$('.Null_filme').hide();
-					
+
 				}else{
 					$('.Null_filme').show();
 					$('#dateTabs').hide();
@@ -520,11 +535,11 @@ $.extend({
 						$(this).addClass('click_li').siblings().removeClass('click_li');
 						$('.list_li_div').find('.list_li_div_ul').removeClass('click_ul').eq(index).addClass('click_ul');
 					})
-					
+
 					var indexs = 0;
 					$('.list_lsi_div').find('.swiper-slide').eq(indexs).addClass('swiper-slide1');
 					function indexs1(index_text){
-						
+
 						var img_wid = $('.list_lsi_div').find('.swiper-slide img').width();
 						$('.list_lsi_div').find('.swiper-slide').removeClass('swiper-slide1').eq(index_text).addClass('swiper-slide1');
 						$('.list_lsi_div').find('.swiper-slide').css({
@@ -544,9 +559,9 @@ $.extend({
 								modifier: 1,
 								slideShadows : true
 							},
-							
-							
-							
+
+
+
 							//onSlideChangeEnd: function(mySwiper1){
 							onTransitionEnd: function(mySwiper1){
 							  //console.log(mySwiper1)
@@ -554,24 +569,24 @@ $.extend({
 							   indexs = mySwiper1.activeIndex;
 							  indexs1(indexs)
 							  touchChange(indexs);
-							  
+
 							} */
 							 slidesPerView : 4,
 							centeredSlides : true,
-							
-							longSwipesRatio : 0.1, 
-							
+
+							longSwipesRatio : 0.1,
+
 							onSlideChangeEnd: function(mySwiper1){
 								indexs = mySwiper1.activeIndex;
 							  indexs1(indexs)
 								touchChange(indexs);
-								  
+
 								//fixPagesHeight(0);
 							},
 							onSlideTouch: function(mySwiper1){
 								//alert(22)
 								mySwiper1.swipeTo(mySwiper1.activeIndex);
-								
+
 							}
 
 						})
@@ -581,7 +596,7 @@ $.extend({
 								"width":img_wid
 						})
 
-				
+
 				var mySwiper = new Swiper('.swiper_body',{
 				 		effect: 'coverflow',
 				        grabCursor: true,
@@ -626,8 +641,8 @@ $.extend({
 							//alert( mySwiper_home.activeIndex);
 							obj_index1 = mySwiper_home.activeIndex;
 							touchChange1(obj_index1);
-								
-								  
+
+
 								//fixPagesHeight(0);
 							}
 
@@ -640,11 +655,11 @@ $.extend({
 
 
 			}
-		},'json');	
+		},'json');
 
 
 	}
-	
+
 })
 
  		$('#swiper-wrapper').on('click','.click_ul li',function(){
@@ -655,7 +670,7 @@ $.extend({
 			rq = $(this).find('input[type="hidden"]').eq(3).val();
 			xq = $(this).find('input[type="hidden"]').eq(4).val();
 			nm = $(this).find('input[type="hidden"]').eq(5).val();
-			
+
 			//alert(rq);
 			$('#pid').val(pid);
 			$('#id').val(id);
@@ -665,7 +680,7 @@ $.extend({
 			$('.p3_h3 span').html(rq);
 			var wid_hei = $(window).height(),
 				obj_hei = $('.Pop_up_h2_body').height();
-				
+
 			$(this).addClass('OK_li').siblings().removeClass('OK_li');
 			if((yc=='user120002904'&&nm=='情侣厅')||(yc=='user120002904'&&nm=='VIP厅')){
 				alert('该场次暂不售卖');return false;
@@ -677,12 +692,12 @@ $.extend({
 					"top":wid_hei/2-obj_hei-2
 				})
 			}
-			
-			
+
+
 			$("body").eq(0).addClass('over_hide');
-			
-			
-			
+
+
+
 		})
 
 
