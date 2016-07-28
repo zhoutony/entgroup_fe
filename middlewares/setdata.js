8273a228ec68bc89994af26788413a01fe72001c
bingosir <1457955164@qq.com>
2016-07-28 10:33:35 +0800

    入口 文件调整

--- middlewares/setdata.js
+++ middlewares/setdata.js
@@ -2,22 +2,28 @@ import express from 'express';
 import fetch from 'isomorphic-fetch';
 const router = express.Router();
 
-const setdata = router.get('/indexInit/yc/:yc', (req, res, next) => {
-	//console.log(req.params.yc);
+const setdata = (req, res, next) => {
+	//console.log(req.query.yc);
+	var cid=req.session.cinemaid;
+	if(cid){
+		return next();
+	}else{
+		var yc=req.query.yc;
+		fetch(api_url+'selciname/bycinemaID?cinemaID='+yc)
+		.then(response => response.json())
+		.then(book => {
+			//console.log(book.resl[0]['cinemaid']);
+			req.session.cinemaid = book.resl[0]['cinemaid'];//写入至session   影院id
+			 return next();
 
-	fetch(api_url+'selciname/bycinemaID?cinemaID='+req.params.yc)
-	.then(response => response.json())
-	.then(book => {
-		//console.log(book.resl[0]['cinemaid']);
-		req.session.cinemaid = book.resl[0]['cinemaid'];//写入至session   影院id
-		 return next();
+			//console.log(req.session.cinemaid);
+		});		
+	}
 
-		//console.log(req.session.cinemaid);
-	});
 
 	
 	
 	
-});
+};
 
 export default setdata;
