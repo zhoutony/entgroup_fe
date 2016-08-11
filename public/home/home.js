50a76a641428604d53eb7a41217f0203a58b12a5
FalseMask <hfgb_92@163.com>
2016-08-11 15:41:24 +0800

    控制器修改

--- public/home/home.js
+++ public/home/home.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
-import { foo } from "./swiper.js";  
+
+import { foo } from "./swiper.js";
 
 import { footer } from "./footer.js";
 import { film } from "./Flime_init.js";
+import { Video_details } from "./Video_details.js";
