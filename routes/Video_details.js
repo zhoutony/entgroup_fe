2be3a8f942bf8bf6ebb344a2043a4f886737822d
FalseMask <hfgb_92@163.com>
2016-08-11 15:32:41 +0800

    影片详情数据调用

--- routes/Video_details.js
+++ routes/Video_details.js
@@ -6,8 +6,22 @@ const router = express.Router();
 
 //活动列表
 router.get('/:film_id', (req, res, next) => {
- // console.log(req.params.film_id);
-	res.render('Video_details/Video_details');
+  console.log();
+	//res.render('Video_details/Video_details');
+  fetch(api_url+'movie/getmoviedetail?movieID=2')
+  .then(response => response.json())
+  .then(Video_details => {
+    //console.log(Video_details);
+      //res.render('Video_details/Video_details', { Video_details:Video_details.resl });
+      fetch(api_url+'selmoviecomment/byMovieID?MovieID=1')
+      .then(response => response.json())
+      .then(byMovie => {
+        console.log(Video_details);
+          res.render('Video_details/Video_details', { byMovie:byMovie.resl,Video_details:Video_details.resl });
+      });
+  });
+
+
 });
 
 
