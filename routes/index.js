c0e4f572a1f84d6c597268672e0bc4d75edaadbb
bingosir <1457955164@qq.com>
2016-09-02 11:41:01 +0800

    微信 jsdk  地图功能      只能在线上测试才可以

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ import filmnews from './filmnews';
 import pay from './pay';
 import cinema from './cinema';
 import Video_details from './Video_details';
-//import sign from './check_sign';
+import sign from './check_sign';
 
 import setdata from '../middlewares/setdata';
 
@@ -29,7 +29,7 @@ router.use('/weixin',weixin);
 router.use('/pay',pay);
 router.use('/cinema',cinema);
 router.use('/Video_details',Video_details);
-//router.use('/sign',sign);
+router.use('/sign',sign);
 
 export default router;
 
