111d1d37d9d36c614439da5eb53f85cdc5f7f59f
FalseMask <hfgb_92@163.com>
2016-09-05 11:32:52 +0800

    发现-新影片分类ＪＳ，样式修改

--- public/active/reset.js
+++ public/active/reset.js
@@ -1,1 +1,6 @@
-
+import $ from 'jquery';
+$(".icon_menu_ul li").on("click",function(){
+    $(this).addClass("on").siblings(".on").removeClass('on');
+    var index = $(this).index();
+    $(".film-list").find("ul").eq(index).addClass("show_ul").siblings().removeClass('show_ul')
+})
