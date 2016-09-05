504fa3714c3ce8deec5f16e31b1f17386d417af1
FalseMask <hfgb_92@163.com>
2016-09-05 17:39:28 +0800

    影片列表选择场次页 - 添加操作优惠活动ＪＳ

--- public/home/home.js
+++ public/home/home.js
@@ -4,4 +4,5 @@ import { foo } from "./swiper.js";
 
 import { footer } from "./footer.js";
 import { film } from "./Flime_init.js";
+import { click_gg } from "./Flime_click.js";
 import { Video_details } from "./Video_details.js";
