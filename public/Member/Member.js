cfd7bbca9bf8a7d8709b208479524bb537e35383
FalseMask <hfgb_92@163.com>
2016-09-07 17:52:49 +0800

    绑定会员卡-样式与显示隐藏密码 错误弹出框 的JS添加

--- public/Member/Member.js
+++ public/Member/Member.js
@@ -1,1 +1,46 @@
-alert(222)
+import $ from 'jquery';
+
+/*****点击绑定会员卡弹出框提示***/
+function Pop_Up1(){
+  $(".pop_up").remove();
+
+}
+function Pop_Up(text){
+  var html = '';
+    html+='<div class="pop_up">';
+      html+='<div class="mack"></div>';
+      html+='<div class="error-btn">';
+        html+='<a>'+text+'<br>请重新输入</a>';
+      html+='</div>';
+    html+='</div>';
+$(".box").append(html);
+var win_hei = $(window).height(),
+    obj_hei = $(".error-btn").height();
+    $(".error-btn").css({
+      "top":win_hei/2-obj_hei/2
+    })
+     setTimeout(Pop_Up1,2000);
+}
+
+var time;
+$(".enter-btn").on("click",function(){
+  new Pop_Up("密码错误")
+
+})
+
+/*****显示密码和隐藏密码****/
+var hide_show = 0;
+$(".X_S").on("click",function(){
+  hide_show++;
+  if(hide_show%2==1){
+     $(this).siblings('input').attr("type","text")
+  }else{
+    $(this).siblings('input').attr("type","password")
+  }
+})
+
+/*****删除密码****/
+$(".C_H").on("click",function(){
+  $(this).siblings('input').val('')
+})
+
