918653ba22c7802da32e9fffe332e1e56c103170
FalseMask <hfgb_92@163.com>
2016-09-07 17:56:29 +0800

    订单支付方式的优惠信息样式和优惠信息JS的删除

--- public/pay/payment.js
+++ public/pay/payment.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import { Slide_tad } from './Slide_tad.js';
 $(function(){
 	$('.user_quan_s').hide();
 })
@@ -59,4 +60,16 @@ $(".bottom_ul li").on("click",".Btn_hide",function(){
     $(this).addClass("Btn_check").closest("li").siblings('li').find(".Btn_check").removeClass('Btn_check')
 })
 
-
+/*******更换会员卡支付插件*******/
+var area1 = new LArea();
+    area1.init({
+        'trigger': '#hy_ka_Btn', //触发选择控件的文本框，同时选择完毕后name属性输出到该位置
+        'valueTo': '#input_hide', //选择完毕后id属性输出到该位置
+        'keys': {
+            id: 'id',
+            name: 'name'
+        }, //绑定数据源相关字段 id对应valueTo的value属性输出 name对应trigger的value属性输出
+        'type': 1, //数据源类型
+        'data': [{"id":"1","name":"金卡101212121"},{"id":"2","name":"银卡卡101212121"},{"id":"3","name":"银卡去去去卡101212121"}] //数据源
+    });
+    area1.value=[1,0,0];//控制初始位置，注意：该方法并不会影响到input的value
