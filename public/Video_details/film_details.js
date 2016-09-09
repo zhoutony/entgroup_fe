7e2eb9086654a7b7601a62d77213c194032f84b5
FalseMask <hfgb_92@163.com>
2016-09-09 15:04:53 +0800

    影片详情-影片简介字段截取

--- public/Video_details/film_details.js
+++ public/Video_details/film_details.js
@@ -1,1 +1,16 @@
 import $ from 'jquery';
+var html = $("#hide_input").val(),
+    html_hide = html.substring(0,30);
+$(".span2").html(html_hide+"......");
+$(".span2_1").html(html);
+var index = 0;
+$(".Btn_p").on("click",function(){
+    index++;
+    if(index%2==1){
+        $(".span2_1").show().siblings('.span2').hide();
+    }else{
+       $(".span2").show().siblings('.span2_1').hide();
+    }
+
+})
+
